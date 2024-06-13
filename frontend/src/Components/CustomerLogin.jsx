6f4b0a0d58b2e6098abb563b49b741440a094c0e
Adarsh <aadi.vodnala@gmail.com>
2024-06-13 17:45:06 -0500

    import statements

--- frontend/src/Components/CustomerLogin.jsx
+++ frontend/src/Components/CustomerLogin.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { backendUrl } from '../constants'
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setUser, setToken } from '../store/reducers/authSlice';
 import Cookies from 'js-cookie';
 
